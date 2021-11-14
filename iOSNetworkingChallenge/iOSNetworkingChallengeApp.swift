88b255c092622deb130a9261f7f592d640d22a34
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-11-14 12:24:10 -0500

    Initial Commit

---
+++ iOSNetworkingChallenge/iOSNetworkingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSNetworkingChallengeApp.swift
+//  iOSNetworkingChallenge
+//
+//  Created by Jeffrey Sweeney on 11/14/21.
+//
+
+import SwiftUI
+
+@main
+struct iOSNetworkingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
