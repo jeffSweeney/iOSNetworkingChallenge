b683793fdf6c1af3b3e9a9c677c66092c8abeeb3
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-11-14 16:31:35 -0500

    Implement LectureModel view model to decode and define the Lectures for the app

--- iOSNetworkingChallenge/ViewModels/LectureModel.swift
+++ iOSNetworkingChallenge/ViewModels/LectureModel.swift
@@ -9,6 +9,54 @@
 
 import Foundation
 
-class LectureModel {
-    // TODO: Implement
+class LectureModel: ObservableObject {
+    @Published var lectures = [Lecture]()
+    
+    init() {
+          ingestLectures()
+    }
+    
+    private func ingestLectures() {
+        // Get URL from hosted JSON
+        let url = URL(string: Constants.HostedJSON)
+        
+        // Guard against nil
+        guard url != nil else {
+            return
+        }
+        
+        // Get the URLRequest
+        let urlRequest = URLRequest(url: url!)
+        
+        // Define and start the task
+        let session = URLSession.shared
+        let task = session.dataTask(with: urlRequest, completionHandler: {(data, request, error) in
+            
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            
+            // Data should be defined if reached. Guard just in case.
+            guard data != nil else {
+                print("Data unexpectedly nil")
+                return
+            }
+            
+            // Decode JSON
+            let decoder = JSONDecoder()
+            do {
+                let results = try decoder.decode([Lecture].self, from: data!)
+                
+                // Force main thread to update the published field
+                DispatchQueue.main.async {
+                    self.lectures += results
+                }
+            } catch {
+                print(error)
+            }
+        })
+        
+        task.resume()
+    }
 }
