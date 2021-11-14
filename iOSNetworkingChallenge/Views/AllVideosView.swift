123ad1c6efbcf2eb9aadd1702eca4dcfcd2f25a7
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-11-14 12:29:07 -0500

    Initial commit

--- iOSNetworkingChallenge/Views/AllVideosView.swift
+++ iOSNetworkingChallenge/Views/AllVideosView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  AllVideosView.swift
 //  iOSNetworkingChallenge
 //
 //  Created by Jeffrey Sweeney on 11/14/21.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct AllVideosView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        AllVideosView()
     }
 }
--- iOSNetworkingChallenge/iOSNetworkingChallengeApp.swift
+++ iOSNetworkingChallenge/iOSNetworkingChallengeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct iOSNetworkingChallengeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AllVideosView()
         }
     }
 }
