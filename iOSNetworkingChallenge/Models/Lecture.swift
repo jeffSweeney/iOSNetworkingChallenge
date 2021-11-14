bb597c24baae8d971b14534b6cd64611b5471842
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-11-14 13:15:26 -0500

    Build skeleton of Model and ViewModel

---
+++ iOSNetworkingChallenge/Models/Lecture.swift
@@ -0,0 +1,14 @@
+//
+//  Lecture.swift
+//  iOSNetworkingChallenge
+//
+//  Represents a Model in the app
+//
+//  Created by Jeffrey Sweeney on 11/14/21.
+//
+
+import Foundation
+
+struct Lecture {
+    // TODO: Implement
+}
---
+++ iOSNetworkingChallenge/ViewModels/LectureModel.swift
@@ -0,0 +1,14 @@
+//
+//  LectureModel.swift
+//  iOSNetworkingChallenge
+//
+//  Represents the ViewModel of the app
+//
+//  Created by Jeffrey Sweeney on 11/14/21.
+//
+
+import Foundation
+
+class LectureModel {
+    // TODO: Implement
+}
