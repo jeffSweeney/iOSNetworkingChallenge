14fad61c5947e6d87b55f5e3f51def840018d252
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-11-14 13:15:44 -0500

    Define Constants.HostedJSON

---
+++ iOSNetworkingChallenge/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  iOSNetworkingChallenge
+//
+//  Created by Jeffrey Sweeney on 11/14/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let HostedJSON = "https://jeffsweeney.github.io/iOSNetworkingChallenge_Pages/Data.json"
+}
