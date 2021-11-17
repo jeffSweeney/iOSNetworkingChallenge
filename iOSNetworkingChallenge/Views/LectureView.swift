d9acf17717cf5e2c5c9199e240df4fbed49813fb
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2021-11-16 19:41:24 -0500

    Implement the LectureView with a  video player and title above it

--- iOSNetworkingChallenge/Views/LectureView.swift
+++ iOSNetworkingChallenge/Views/LectureView.swift
@@ -6,13 +6,27 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct LectureView: View {
     let lecture: Lecture
     
     var body: some View {
-        // TODO: Implement video player
-        Text("You've reached the \(lecture.title) lecture")
-            .padding()
+        VStack (spacing: 30) {
+            // Title
+            Text(lecture.title)
+                .bold()
+                .font(.title)
+                .padding()
+            
+            // Video
+            if let url = URL(string: lecture.url) {
+                VideoPlayer(player: AVPlayer(url: url))
+            } else {
+                Text("Error: Unable to Load Video")
+            }
+            
+            Spacer()
+        }
     }
 }
